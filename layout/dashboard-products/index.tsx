94f1a61f886e1d53e82f237081a0bfa1179ff429
renat <renatkapenov@yandex.ru>
2025-10-20 11:50:47 +0500

    [products] updates

--- layout/dashboard-products/index.tsx
+++ layout/dashboard-products/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getProducts} from "@/store/reducers/products/products";
 import {AppDispatch, RootState} from "@/store";
@@ -10,6 +10,8 @@ interface Props {
 function DashboardProducts(props: Props) {
     const dispatch = useDispatch<AppDispatch>();
     const product = useSelector((state: RootState) => state.products.product);
+
+    const [search, setSearch] = useState<string>('');
 
     useEffect(() => {
         dispatch(getProducts({page: 1, limit: 20}))
@@ -23,8 +25,18 @@ function DashboardProducts(props: Props) {
             });
     }, []);
 
+    function searchProduct(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch(e.target.value);
+    }
+
     return (
         <div className="dashboard_products">
+            <div className="input-group input-group-lg">
+                <span className="input-group-text" id="product-search">Поиск</span>
+                <input type="text" className="form-control" aria-label="Sizing example input"
+                       aria-describedby="inputGroup-sizing-lg" value={search} onChange={searchProduct}/>
+            </div>
+
             {
                 product.items.length <= 0 ? <div>Wait</div> : <table className="table">
                     <thead>
@@ -38,16 +50,16 @@ function DashboardProducts(props: Props) {
                     </thead>
                     <tbody className="table-group-divider">
                     {product.items.map(item => (
-                            <tr>
-                                <th scope="row">
-                                    {item.imageUrl ? <img src={item.imageUrl} alt=""/> : 'not imgae'}
-                                </th>
-                                <td>{item.name}</td>
-                                <td>{item.article}</td>
-                                <td>{item.kaspiPrice}</td>
-                                <td><a href={item.kaspiLink}>Каспи</a></td>
-                            </tr>
-                        ))}
+                        <tr key={item._id}>
+                            <th scope="row">
+                                {item.imageUrl ? <img src={item.imageUrl} alt=""/> : 'not imgae'}
+                            </th>
+                            <td>{item.name}</td>
+                            <td>{item.article}</td>
+                            <td>{item.kaspiPrice}</td>
+                            <td><a href={item.kaspiLink}>Каспи</a></td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
             }
